36f944030ea2036ea435aef8b2b9c644b6720e9f
iamhenrika <113649340+iamhenrika@users.noreply.github.com>
2023-07-31 10:34:52 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project is coded by Henrika Smith and is open-sourced on <a
+          href="https://github.com/iamhenrika/react-forecast-app"
+          target="_blank"
+        >Github</a>
+      </footer>
+    </div>
+  );
 }
