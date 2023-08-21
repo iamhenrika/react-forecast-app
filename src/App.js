d347bafd20ea59c249592ec7a0cc87ba171b206f
iamhenrika <113649340+iamhenrika@users.noreply.github.com>
2023-08-21 18:55:41 -0400

    added basic styling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+import React from "react"
 import "./App.css";
+import Weather from "./Weather"
 
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project is coded by Henrika Smith and is open-sourced on <a
-          href="https://github.com/iamhenrika/react-forecast-app"
-          target="_blank"
-        >Github</a>
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          This project is coded by Henrika Smith and is open-sourced on{" "}
+          <a
+            href="https://github.com/iamhenrika/react-forecast-app"
+            target="_blank"
+          >
+            Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
